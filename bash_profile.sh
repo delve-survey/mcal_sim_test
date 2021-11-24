f6a4fcb5ebd5a2ceca3bb3e1a1d46d6a354dd415
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2021-11-24 12:45:39 -0600

    Path variable file

---
+++ bash_profile.sh
@@ -0,0 +1,5 @@
+
+#source your conda environment first
+#Then source this file as "source bash_profile.sh"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/oracle_instant_client/instantclient_21_1
