e07d4435ce317c1c0a0fa0f4748a511422768f2b
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2021-12-07 17:31:17 -0600

    Env. Variables

--- bash_profile.sh
+++ bash_profile.sh
@@ -3,3 +3,10 @@
 #Then source this file as "source bash_profile.sh"
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/oracle_instant_client/instantclient_21_1
+
+
+#TEMP SOLUTION. DELETE LATER.
+export MEDS_DIR=/home/dhayaa/Desktop/DECADE/TEMP_MEDS/
+
+#Need this to point python to desmeds master file
+export PYTHONPATH=/home/dhayaa/Desktop/DECADE/desmeds_master:$PYTHONPATH
