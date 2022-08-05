b6dc2384260d06aa412b27a0bfc46a2e2c85e08c
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2022-08-05 11:24:09 -0500

    Split pre-metacal operations + Config file setup

--- run_metacal.py
+++ run_metacal.py
@@ -50,8 +50,7 @@ def run_metacal(*, tilename, output_meds_dir, bands, seed, mcal_config):
     if n_obj_per_chunk * n_chunks < cat.size:
         n_obj_per_chunk += 1
     assert n_obj_per_chunk * n_chunks >= cat.size
-    logger.info(
-        ' running metacal for %d objects in %d chunks', cat.size, n_chunks)
+    logger.info(' running metacal for %d objects in %d chunks', cat.size, n_chunks)
 
     seeds = np.random.RandomState(seed=seed).randint(1, 2**30, size=n_chunks)
 
@@ -128,8 +127,10 @@ def _run_mcal_one_chunk(meds_files, start, end, seed, mcal_config):
             o = _strip_coadd(o, mcal_config) #Remove coadd since it isnt used in fitting
             o = _strip_zero_flux(o, mcal_config) #Remove any obs with zero flux
             
-            if mcal_config['custom']['interp_bad_pixels']:
-                o = _fill_empty_pix(o, rng, mcal_config) #Interpolate empty pixels (and remove img where we cant do it properly)
+            if mcal_config['custom']['maxbadfrac'] > 0: o = _strip_10percent_masked(o, mcal_config) #Remove obs with many bad pixs
+            if mcal_config['custom']['goodfrac']: o = _get_masked_frac(o, mcal_config) #Get gauss-weighted fraction of good pixels
+            if mcal_config['custom']['symmetrize_mask']: o = _symmetrize_mask(o, mcal_config) #Symmetrize bmask for 180 deg symmetry
+            if mcal_config['custom']['interp_bad_pixels']: o = _fill_empty_pix(o, rng, mcal_config) #Interpolate empty pixels
             
             o = _apply_pixel_scale(o, mcal_config) #Not sure??
 
@@ -143,11 +144,11 @@ def _run_mcal_one_chunk(meds_files, start, end, seed, mcal_config):
 
             o.meta['id'] = ind
             o[0].meta['Tsky'] = 1
-            o[0].meta['magzp_ref'] = MAGZP_REF
+            o[0].meta['magzp_ref']   = MAGZP_REF
             o[0][0].meta['orig_col'] = cat['orig_col'][ind, 0]
             o[0][0].meta['orig_row'] = cat['orig_row'][ind, 0]
 
-            if mcal_config['custom']['interp_bad_pixels']:
+            if mcal_config['custom']['goodfrac']:
                 #put all the good_fraction numbers into one list
                 #one entry per cutout (so all bands are combined here)
                 good_frac = []
@@ -168,12 +169,10 @@ def _run_mcal_one_chunk(meds_files, start, end, seed, mcal_config):
                 res = None
 
             if res is not None:
-                if mcal_config['custom']['interp_bad_pixels']:
-#                     print("I DID THINGS!!!!!")
+                if mcal_config['custom']['goodfrac']:
                     res['good_frac'] = np.average(good_frac, weights = weight) #store mean good_fraction per object
                 else:
-#                     print("I DID NOTHING!")
-                    res['good_frac'] = 1 #Else, we're assuming image is "perfect" == 1
+                    res['good_frac'] = 1 #Else, we're assuming image is "perfect" (completely unmasked) == 1
                 data.append(res)
 
         if len(data) > 0:
@@ -224,8 +223,31 @@ def _apply_pixel_scale(mbobs, mcal_config):
             
     return mbobs
 
+def _strip_10percent_masked(mbobs, mcal_config):
+    _mbobs = MultiBandObsList()
+    _mbobs.update_meta_data(mbobs.meta)
+    
+    #Loop over different band observations (r, i, z)
+    for ol in mbobs:
+        _ol = ObsList()
+        _ol.update_meta_data(ol.meta)
+        
+        #Loop over different exposures/cutouts in each band
+        for i in range(len(ol)):
+            
+            msk = ol[i].bmask.astype(bool) #Mask where TRUE means bad pixel
+            
+#             print("strip", np.average(msk), mcal_config['custom']['maxbadfrac'])
+                
+            if np.average(msk) >= mcal_config['custom']['maxbadfrac']:
+                continue
+            
+            _ol.append(ol[i])
+        _mbobs.append(_ol)
+    return _mbobs
 
-def _fill_empty_pix(mbobs, rng, mcal_config):
+def _get_masked_frac(mbobs, mcal_config):
+    
     _mbobs = MultiBandObsList()
     _mbobs.update_meta_data(mbobs.meta)
     
@@ -256,6 +278,55 @@ def _fill_empty_pix(mbobs, rng, mcal_config):
             ol[i].meta['good_frac'] = good_frac
             ol[i].meta['weight']    = wgt
             
+#             print("goodfrac", good_frac, wgt)
+
+            _ol.append(ol[i])
+        _mbobs.append(_ol)
+    return _mbobs
+
+def _symmetrize_mask(mbobs, mcal_config):
+    
+    _mbobs = MultiBandObsList()
+    _mbobs.update_meta_data(mbobs.meta)
+    
+    #Loop over different band observations (r, i, z)
+    for ol in mbobs:
+        _ol = ObsList()
+        _ol.update_meta_data(ol.meta)
+        
+        #Loop over different exposures/cutouts in each band
+        for i in range(len(ol)):
+            
+            msk = ol[i].bmask.astype(bool) #Mask where TRUE means bad pixel
+                
+#             print("symm1", msk.sum(), np.sum(ol[i].bmask))
+            #Rotate because Metacal needs this
+            msk |= np.rot90(msk, k = 1)
+            
+            #Write rotated mask back to observation
+            ol[i].bmask = msk.astype(np.int32)
+            
+#             print("symm2", msk.sum(), np.sum(ol[i].bmask))
+            
+            _ol.append(ol[i])
+        _mbobs.append(_ol)
+    return _mbobs
+    
+def _fill_empty_pix(mbobs, rng, mcal_config):
+    _mbobs = MultiBandObsList()
+    _mbobs.update_meta_data(mbobs.meta)
+    
+    #Loop over different band observations (r, i, z)
+    for ol in mbobs:
+        _ol = ObsList()
+        _ol.update_meta_data(ol.meta)
+        
+        #Loop over different exposures/cutouts in each band
+        for i in range(len(ol)):
+            
+            msk = ol[i].bmask.astype(bool) #Mask where TRUE means bad pixel
+            wgt = np.median(ol[i].weight[ol[i].weight != 0]) #Median weight used to populate noise in empty pix
+            
             #Observation doesn't have noise image, and so add noise image in.
             #Just random gaussian noise image using weights
             #Need to do this for interpolation step    
@@ -263,28 +334,22 @@ def _fill_empty_pix(mbobs, rng, mcal_config):
 
             
             #If there are any bad mask pixels, then do interpolation
-#             if False:
             if np.any(msk):
                 
-                #Rotate because Metacal needs this
-                msk |= np.rot90(msk, k = 1)
-
-                #Interpolate image to fill in gaps
+                #Interpolate image to fill in gaps. Setting maxfrac=1 since maxfrac is checked beforehand
                 im    = interpolate_image_at_mask(image=ol[i].image, weight=wgt, bad_msk=msk, 
-                                                  rng=rng, maxfrac=0.1, buff=4,
+                                                  rng=rng, maxfrac=1, buff=4,
                                                   fill_isolated_with_noise=True)
 
                 #Interpolate over noise image
                 noise = interpolate_image_at_mask(image=ol[i].noise, weight=wgt, bad_msk=msk, 
-                                                  rng=rng, maxfrac=0.1, buff=4,
+                                                  rng=rng, maxfrac=1, buff=4,
                                                   fill_isolated_with_noise=True)
 
                 #If we can't interpolate image or noise due to lack of data
                 #then we skip this observation (it is stripped from MultiBandObs list)
                 if (im is None) | (noise is None):
                     continue
-#                 if (im is None):
-#                     continue
                     
                 #Set all masked pixel weights to 0.0
                 ol[i].image  = im
