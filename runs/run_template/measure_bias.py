d528cfb802634b8c111630ddca079f039a9c7fec
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2022-08-06 01:56:15 -0500

    Correct noise trick

--- runs/run_template/measure_bias.py
+++ runs/run_template/measure_bias.py
@@ -6,6 +6,11 @@ import sys
 
 from render_jobs import tilenames
 
+
+summary = np.mean
+
+tilenames = tilenames[:5]
+
 print('--------------------------')
 print('USING TILES:')
 print('--------------------------')
@@ -24,82 +29,87 @@ print(PATH)
 tiles_list = [[x] for x in tilenames]
 tiles_list.append(tilenames)
 
+seed_list = [[i] for i in range(len(tilenames))]
+seed_list.append([i for i in range(len(tilenames))])
+
+def _get_subset_array(res, i, string):
+    '''
+    Convenience function for getting reading out specific arrays
+    and only for specific objects that are part of a jackknife "patch"
+    '''
+    ind = np.where(Results['batch_num_%s'%string[3:]] != i)[0] #Select only objects in given batch/subset
+    
+    return res[string][ind]
+
 #Loop over tiles (and also a combination of all tiles together)
 #and get m and c for objects in each tile
-for tiles in tiles_list:
-    N = len(tiles)
+for tiles, seed in zip(tiles_list, seed_list):
     
+    N = len(tiles) 
+    
+    if N == 1: continue #Just quick "hack" so we skip all single tile calculations
     
     print("--------------------------------------------------------")
     print("TILES:", tiles)
     print("--------------------------------------------------------")
-
-    gplus  = [fits.open(PATH + '/metacal_%s_seed0_gplus.fits'%t) for t in tiles]
-    gminus = [fits.open(PATH + '/metacal_%s_seed0_gminus.fits'%t) for t in tiles]
-
-    shear = '_noshear'
-    Flags_plus = np.concatenate([gplus[i][1].data['mcal_flags'] for i in range(N)])
-    SNR_plus   = np.concatenate([gplus[i][1].data['mcal_s2n' + shear] for i in range(N)])
-    T_plus     = np.concatenate([gplus[i][1].data['mcal_T_ratio' + shear] for i in range(N)])
     
-    Mask_plus  = (Flags_plus == 0) & (SNR_plus > 10) & (T_plus > 0.5)
+    gplus  = [fits.open(PATH + '/metacal_%s_seed%d_gplus.fits'%(t, i)) for t, i in zip(tiles, seed)]
+    gminus = [fits.open(PATH + '/metacal_%s_seed%d_gminus.fits'%(t, i)) for t, i in zip(tiles, seed)]
     
-    print("FRACTION KEPT (PLUS) :", Mask_plus.sum()/Mask_plus.size)
     
-    Flags_minus = np.concatenate([gminus[i][1].data['mcal_flags'] for i in range(N)])
-    SNR_minus   = np.concatenate([gminus[i][1].data['mcal_s2n' + shear] for i in range(N)])
-    T_minus     = np.concatenate([gminus[i][1].data['mcal_T_ratio' + shear] for i in range(N)])
+    ngrid = 10
     
-    Mask_minus  = (Flags_minus == 0) & (SNR_minus > 10) & (T_minus > 0.5)
-    print("FRACTION KEPT (MINUS):",Mask_minus.sum()/Mask_minus.size)
+    spacing = 10_000/ngrid
+    Results = {}
     
-    e1_plus_array = np.concatenate([gplus[i][1].data['mcal_g_noshear'][:,0] for i in range(N)])[Mask_plus]
-    e2_plus_array = np.concatenate([gplus[i][1].data['mcal_g_noshear'][:,1] for i in range(N)])[Mask_plus]
+    for g_name, g in zip(['p', 'm'], [gplus, gminus]):
+        
+        Flags = np.concatenate([g[i][1].data['mcal_flags'] for i in range(N)])
 
-    e1_minus_array = np.concatenate([gminus[i][1].data['mcal_g_noshear'][:,0] for i in range(N)])[Mask_minus]
-    e2_minus_array = np.concatenate([gminus[i][1].data['mcal_g_noshear'][:,1] for i in range(N)])[Mask_minus]
-
-
-    R11_plus_array = np.concatenate([(gplus[i][1].data['mcal_g_1p'][:,0] - gplus[i][1].data['mcal_g_1m'][:,0])/0.02 for i in range(N)])[Mask_plus]
-    R22_plus_array = np.concatenate([(gplus[i][1].data['mcal_g_2p'][:,1] - gplus[i][1].data['mcal_g_2m'][:,1])/0.02 for i in range(N)])[Mask_plus]
-
-
-    R11_minus_array = np.concatenate([(gminus[i][1].data['mcal_g_1p'][:,0] - gminus[i][1].data['mcal_g_1m'][:,0])/0.02 for i in range(N)])[Mask_minus]
-    R22_minus_array = np.concatenate([(gminus[i][1].data['mcal_g_2p'][:,1] - gminus[i][1].data['mcal_g_2m'][:,1])/0.02 for i in range(N)])[Mask_minus]
+        for s in ['noshear', '1p', '1m', '2p', '2m']:
+              
+            
+            SNR = np.concatenate([g[i][1].data['mcal_s2n_%s'%s] for i in range(N)])
+            T   = np.concatenate([g[i][1].data['mcal_T_ratio_%s'%s] for i in range(N)])
+            Mask = (Flags == 0) & (SNR > 10) & (T > 0.5)
+            
+            Results['e1_%s_%s'%(g_name, s)] = np.concatenate([g[i][1].data['mcal_g_%s'%s][:,0] for i in range(N)])[Mask]
+            Results['e2_%s_%s'%(g_name, s)] = np.concatenate([g[i][1].data['mcal_g_%s'%s][:,1] for i in range(N)])[Mask]
+            
+            Results['batch_num_%s_%s'%(g_name, s)] = np.concatenate([
+                                                        g[i][1].data['x']//spacing + 
+                                                        g[i][1].data['y']//spacing * ngrid + 
+                                                        i * ngrid**2 
+                                                        for i in range(N)]).astype(int)[Mask]
 
     del gplus, gminus
+    
 
-    nBoot = 1000
+    Npatch = ngrid**2*len(tilenames)
+    N = np.arange(Npatch)
+#     N = np.random.choice(N, 200, replace = False)
+    e1_plus  = np.zeros(N.size)
+    e1_minus = np.zeros(N.size)
+    e2_plus  = np.zeros(N.size)
+    e2_minus = np.zeros(N.size)
 
-    e1_plus  = np.zeros(nBoot)
-    e1_minus = np.zeros(nBoot)
-    e2_plus  = np.zeros(nBoot)
-    e2_minus = np.zeros(nBoot)
-
-    R11_plus  = np.zeros(nBoot)
-    R11_minus = np.zeros(nBoot)
-    R22_plus  = np.zeros(nBoot)
-    R22_minus = np.zeros(nBoot)
+    R11_plus  = np.zeros(N.size)
+    R11_minus = np.zeros(N.size)
+    R22_plus  = np.zeros(N.size)
+    R22_minus = np.zeros(N.size)
 
     
-    print("SIZE : ", e1_plus_array.shape)
+    for i in tqdm(range(N.size)):
 
-
-    for i in tqdm(range(nBoot)):
-
-        index_plus  = np.random.randint(0, len(e1_plus_array),  len(e1_plus_array))
-        index_minus = np.random.randint(0, len(e1_minus_array), len(e1_minus_array))
-
-        e1_plus[i]  = np.mean(e1_plus_array[index_plus])
-        e1_minus[i] = np.mean(e1_minus_array[index_minus])
-        e2_plus[i]  = np.mean(e2_plus_array[index_plus])
-        e2_minus[i] = np.mean(e2_minus_array[index_minus])
-
-        R11_plus[i]   = np.mean(R11_plus_array[index_plus])
-        R11_minus[i]  = np.mean(R11_minus_array[index_minus])
-        R22_plus[i]   = np.mean(R22_plus_array[index_plus])
-        R22_minus[i]  = np.mean(R22_minus_array[index_minus])
-
+        e1_plus[i]  = summary(_get_subset_array(Results, N[i], 'e1_p_noshear'))
+        e1_minus[i] = summary(_get_subset_array(Results, N[i], 'e1_m_noshear'))
+        e2_plus[i]  = summary(_get_subset_array(Results, N[i], 'e2_p_noshear'))
+        e2_minus[i] = summary(_get_subset_array(Results, N[i], 'e2_m_noshear'))
+        
+        R11_plus[i]  = (summary(_get_subset_array(Results, N[i], 'e1_p_1p')) - summary(_get_subset_array(Results, N[i], 'e1_p_1m')))/0.02
+        R11_minus[i] = (summary(_get_subset_array(Results, N[i], 'e1_m_1p')) - summary(_get_subset_array(Results, N[i], 'e1_m_1m')))/0.02
+        R22_plus[i]  = (summary(_get_subset_array(Results, N[i], 'e2_p_2p')) - summary(_get_subset_array(Results, N[i], 'e2_p_2m')))/0.02
+        R22_minus[i] = (summary(_get_subset_array(Results, N[i], 'e2_m_2p')) - summary(_get_subset_array(Results, N[i], 'e2_m_2m')))/0.02
 
     R11 = (R11_plus + R11_minus)/2
     R22 = (R22_plus + R22_minus)/2
@@ -112,9 +122,9 @@ for tiles in tiles_list:
 
     print('\n---------------------------------\n')
     print("----------- COMBINED ---------------")
-    print('size      N = %d'%e1_plus_array.shape)
-    print('recovered m = %1.3f +/- %1.3f [1e-3, 3-sigma]'%(np.mean(m/1e-3), 3*np.std(m/1e-3)))
-    print('recovered c = %1.3f +/- %1.3f [1e-5, 3-sigma]'%(np.mean(c/1e-5), 3*np.std(c/1e-5)))
+    print('size      N = %d'%Results['e1_p_noshear'].shape)
+    print('recovered m = %1.3f +/- %1.3f [1e-3, 3-sigma]'%(np.mean(m/1e-3), 3*np.std(m/1e-3)*np.sqrt(Npatch)))
+    print('recovered c = %1.3f +/- %1.3f [1e-5, 3-sigma]'%(np.mean(c/1e-5), 3*np.std(c/1e-5)*np.sqrt(Npatch)))
     
     print('\n---------------------------------\n')
     
@@ -128,9 +138,9 @@ for tiles in tiles_list:
     c = g2
 
     print("----------- PLUS ---------------")
-    print('size      N = %d'%e1_plus_array.shape)
-    print('recovered m = %1.3f +/- %1.3f [1e-3, 3-sigma]'%(np.mean(m/1e-3), 3*np.std(m/1e-3)))
-    print('recovered c = %1.3f +/- %1.3f [1e-5, 3-sigma]\n\n'%(np.mean(c/1e-5), 3*np.std(c/1e-5)))
+    print('size      N = %d'%Results['e1_p_noshear'].shape)
+    print('recovered m = %1.3f +/- %1.3f [1e-3, 3-sigma]'%(np.mean(m/1e-3), 3*np.std(m/1e-3)*np.sqrt(Npatch)))
+    print('recovered c = %1.3f +/- %1.3f [1e-5, 3-sigma]'%(np.mean(c/1e-5), 3*np.std(c/1e-5)*np.sqrt(Npatch)))
     
     R11 = R11_minus
     R22 = R22_minus
@@ -142,9 +152,9 @@ for tiles in tiles_list:
     c = g2
 
     print("----------- MINUS ---------------")
-    print('size      N = %d'%e1_minus_array.shape)
-    print('recovered m = %1.3f +/- %1.3f [1e-3, 3-sigma]'%(np.mean(m/1e-3), 3*np.std(m/1e-3)))
-    print('recovered c = %1.3f +/- %1.3f [1e-5, 3-sigma]'%(np.mean(c/1e-5), 3*np.std(c/1e-5)))
+    print('size      N = %d'%Results['e1_p_noshear'].shape)
+    print('recovered m = %1.3f +/- %1.3f [1e-3, 3-sigma]'%(np.mean(m/1e-3), 3*np.std(m/1e-3)*np.sqrt(Npatch)))
+    print('recovered c = %1.3f +/- %1.3f [1e-5, 3-sigma]'%(np.mean(c/1e-5), 3*np.std(c/1e-5)*np.sqrt(Npatch)))
           
           
     print("------------------------------------------------------------")
